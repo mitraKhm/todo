2bbf2d12606199bbc4f80a879ef3f7af162968d3
mitraKhm <mitra.kh1995@gmail.com>
2021-10-09 10:24:43 +0330

    Initial commit

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Awesome Todo
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+     Navigation
+        </q-item-label>
+        <q-item
+          v-for="task in taskList"
+        >
+          <q-item-section>
+            <q-icon v-if="task.icon">
+            </q-icon>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>
+              {{task.title}}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+
+<!--        <EssentialLink-->
+<!--          v-for="link in essentialLinks"-->
+<!--          :key="link.title"-->
+<!--          v-bind="link"-->
+<!--        />-->
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const taskList = [
+  {
+    title: 'Todo',
+    icon: 'list',
+    link: '/'
+  },
+  {
+    title: 'Settings',
+    icon: 'settings',
+    link: '/setting'
+  },
+];
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: taskList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
